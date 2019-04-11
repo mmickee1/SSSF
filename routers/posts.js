57f6e418c72d6b67abfc91908006c6e9824cd689
Mikael M <mikael_96@live.com>
2019-04-11 20:00:49 +0300

    uploading closer. defining correct fields and using right values still missing, logic there now

--- routers/posts.js
+++ routers/posts.js
@@ -2,6 +2,21 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
+const postModel = require('../models/uploadinfo');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // callback=cb
+    cb(null, './public/uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({
+  storage: storage
+}).single('image');
+
 
 
 //this route is https://localhost:3000/posts/:something
@@ -14,10 +29,48 @@ router.get('/add', (req, res) => {
   res.render('add.pug', { title: 'Add equipment', message: 'Here you can add equipment to be sold!' });
 });
 
+
+//POST========================================================================================================================
+//router.post('/uploadnew', postController.create_post);
 router.post('/upload', (req, res) => {
   //postController.create_post(req.body);
+  upload(req, res, (err) => {
+    if (err) {
+      res.sendStatus(400);
+    } else {
+      if (req.file == undefined) {
+        res.send('no file selected');
+      } else {
+        console.log(req.file);
+        //res.send(req.file);
+        //jsonfile.writeFile(file, req.file, { flag: 'a' })
+        next();
+      }
+    }
+  });
 });
-//router.post('/uploadnew', postController.create_post);
+
+
+//DEFINE THESE HERE: 
+/* uploader: String,
+    category: String,
+    title: String,
+    description: String,
+    imageurl: String,
+    imagename: String*/
+router.use('/upload', (req, res, next) => {
+  UploadInfo.create({
+    category: req.body.category,
+    title: req.body.title,
+    description: req.body.description
+  }).then(c => {
+    res.send('Imagefile uploaded: ' + c.id);
+  }, err => {
+    res.send('Error: ' + err);
+  });
+});
+
+//============================================================================================================================
 
 
 module.exports = router;
